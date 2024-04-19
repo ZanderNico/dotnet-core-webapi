c7017c0a1830d633fd67ec45de3a3af2cea5bd7b
Nico <106797953+ZanderNico@users.noreply.github.com>
2024-04-19 14:20:00 +0800

    Update CountryController.cs

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -87,7 +87,7 @@ namespace dotnet_api.Controllers
 
             if (!_countryRepository.CreateCountry(countryMap))
             {
-                ModelState.AddModelError("", "Something went wrong while savin");
+                ModelState.AddModelError("", "Something went wrong while saving");
                 return StatusCode(500, ModelState);
             }
 
